3498ef21add5fce8c0ef091cde113359a769d194
bburnett86 <breton.burnett@gmail.com>
2016-10-04 16:03:20 -0500

    User auth coplete

--- flapper-news/app/assets/javascripts/app.js
+++ flapper-news/app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-angular.module('flapperNews',['ui.router', 'templates'])
+angular.module('flapperNews',['ui.router', 'templates', 'Devise'])
 .config([
 '$stateProvider',
 '$urlRouterProvider',
@@ -25,6 +25,28 @@ function($stateProvider, $urlRouterProvider) {
           return posts.get($stateParams.id);
         }]
       }
+    });
+    $stateProvider
+    .state('login', {
+      url:'/login',
+      templateUrl: 'auth/_login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
+    });
+    $stateProvider
+    .state('register', {
+      url: '/register',
+      templateUrl: 'auth/_register.html',
+      controller: 'AuthCtrl',
+      onEnter:['$state', 'Auth', function($state, Auth){
+        Auth.currentUser().then(function(){
+          $state.go('home');
+        })
+      }]
     });
 
   $urlRouterProvider.otherwise('home');
